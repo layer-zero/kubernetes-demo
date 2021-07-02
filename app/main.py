362a05d961a9d7dfd723bc0d002dc1585202b6aa
Tom Lijnse <tom.lijnse@layerzero.nl>
2021-07-02 16:28:37 +0200

    Added database connectivity

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from flask import Flask
 import socket
-#from MySQLdb import connect
+from MySQLdb import connect
 
 ip = socket.gethostbyname(socket.gethostname())
 
-#def get_routers():
-#    db = connect(host='172.20.0.200', db='inventory')
-#    c = db.cursor()
-#    c.execute("SELECT * FROM routers")
-#    return c.fetchall()
+def get_routers():
+    db = connect(host='devcor-db-service', db='inventory')
+    c = db.cursor()
+    c.execute("SELECT * FROM routers")
+    return c.fetchall()
     
 
 app = Flask(__name__)
@@ -19,9 +19,9 @@ def home():
         f'Welcome to Cisco DevNet.<br>'
         f'IP address of the server is {ip}.<br><br>'
     )
-#    out += 'List of routers in inventory:<br>'
-#    for r in get_routers():
-#        out += f'-> Hostname: {r[0]}; IP: {r[1]}<br>'
+    out += 'List of routers in inventory:<br>'
+    for r in get_routers():
+        out += f'-> Hostname: {r[0]}; IP: {r[1]}<br>'
     return out
 
 
